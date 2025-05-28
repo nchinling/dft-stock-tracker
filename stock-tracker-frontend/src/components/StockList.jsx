6fd17197011c5d7d289aa907b245466013b8cb40
Ng Chin Ling (RP) <ng_chin_ling@rp.edu.sg>
2025-05-28 18:15:30 +0800

    first commit

---
+++ stock-tracker-frontend/src/components/StockList.jsx
@@ -0,0 +1,71 @@
+import { StockContext } from "../contexts/StockContext";
+import { useContext } from "react";
+import { deleteStock } from "../service/db-service";
+import "./styles/StockList.css";
+
+function StockList({ title }) {
+  const { stocks, setStockList } = useContext(StockContext);
+
+  const handleDelete = async (stockId) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this stock?"
+    );
+    if (!confirmDelete) return;
+
+    try {
+      const response = await deleteStock(stockId);
+      if (response.success) {
+        // Remove from UI
+        const updatedList = stocks.filter((stock) => stock.stockId !== stockId);
+        setStockList(updatedList);
+      } else {
+        alert(response.message);
+      }
+    } catch (err) {
+      alert(`Error deleting stock. ${err}`);
+    }
+  };
+
+  return (
+    <div className="stock-list">
+      <h2>{title}</h2>
+      {Array.isArray(stocks) && stocks.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Quantity</th>
+              <th>Purchase Price</th>
+              <th>Current Price</th>
+              <th>P/L</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {stocks.map((stock, index) => (
+              <tr key={index}>
+                <td>{stock.symbol}</td>
+                <td>{stock.quantity}</td>
+                <td>{stock.purchasePrice}</td>
+                <td>{stock.currentPrice}</td>
+                <td>placeholder</td>
+                <td>
+                  <button
+                    className="delete-button"
+                    onClick={() => handleDelete(stock.stockId)}
+                  >
+                    x
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No stocks added yet</p>
+      )}
+    </div>
+  );
+}
+
+export default StockList;
